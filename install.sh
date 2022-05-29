f7ad8ea2cf4cd313d6d59ceb2e73f9eaed2280af
KangOxford <kang.li@maths.ox.ac.uk>
2022-05-29 02:23:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ echo ">>> install.sh Done."
 # code --install-extension ms-toolsai.jupyter
 # code --install-extension ms-python.python
 # code --install-extension ms-toolsai.jupyter ms-python.python
+code --list-extensions
 code --install-extension ms-toolsai.jupyter && code --install-extension ms-python.python
