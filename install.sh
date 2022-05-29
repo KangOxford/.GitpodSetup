75f585b20bc4f57d98deff5e8de806b8b405ba5f
KangOxford <kang.li@maths.ox.ac.uk>
2022-05-29 02:33:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@ echo ">>> run install.sh "
 # code --install-extension ms-toolsai.jupyter
 # code --install-extension ms-python.python
 # code --install-extension ms-toolsai.jupyter ms-python.python
-echo ">>> rm -rf ~/.vscode-server"
-rm -rf ~/.vscode-server
+# echo ">>> rm -rf ~/.vscode-server"
+# rm -rf ~/.vscode-server
+
 echo ">>> code --list-extensions"
 code --list-extensions
+
 echo ">>> code --install-extension"
 code --install-extension ms-toolsai.jupyter && code --install-extension ms-python.python
-echo ">>> cat ~/.dotfiles.log"
-cat ~/.dotfiles.log
+
+# echo ">>> cat ~/.dotfiles.log"
+# cat ~/.dotfiles.log
