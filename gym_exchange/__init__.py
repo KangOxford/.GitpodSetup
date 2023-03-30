26f0072b2955bea9a275956ded13dc7ed160844b
KangOxford <kang@robots.ox.ac.uk>
2023-03-30 19:18:57 +0800

    Update __init__.py

--- gym_exchange/__init__.py
+++ gym_exchange/__init__.py
@@ -1,3 +1,18 @@
+"""imitation: implementations of imitation and reward learning algorithms."""
+
+from importlib import metadata
+
+try:
+    __version__ = metadata.version("gym_exchange")
+except metadata.PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    pass
+
+
+
+
+# =================================================================
+
 from gym.envs.registration import register
 
 class Config:
